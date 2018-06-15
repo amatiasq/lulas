cec3714c9fe5730f1d48cff23033baa69dbe4ef3
A. Matías Quezada <amatiasq@gmail.com>
2018-06-15 12:17:30 +0200

    chore: Add declaration for new function in state-decorators

--- src/cell/state-decorators.d.ts
+++ src/cell/state-decorators.d.ts
@@ -1,2 +1,3 @@
 export function buffer(constructor: Function): void;
 export function bufferProp<T>(prototype: T, key: string): void;
+export function staticProp<T>(prototype: T, key: string): void;
